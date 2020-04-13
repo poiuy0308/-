76ccfa80e682bbde2a6043284cdfc453702ffa9a
poiuy0308 <63567777+poiuy0308@users.noreply.github.com>
2020-04-13 12:09:34 +0900

    Add files via upload

---
+++ HW3.cpp
@@ -0,0 +1,36 @@
+﻿// HW3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int);
+
+
+int main() {
+    int num;
+
+    cout << "How many nums would U like to plus " << endl;
+
+    cin >> num;
+    cout << endl;
+
+    cout << sum(num);
+    cout << endl;
+
+
+
+}
+int sum(int num) {
+    int temp = 0;
+    int f_num;
+    for (int i = 0; i < num; i++) {
+        cout << " input a num : ";
+        cin >> f_num;
+        cout << endl;
+        temp += f_num;
+    }
+    return temp;
+}
+
