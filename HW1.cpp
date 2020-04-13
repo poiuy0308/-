bc64ce6de5af43dabf1a70619c4d1b5ae0ab695f
poiuy0308 <63567777+poiuy0308@users.noreply.github.com>
2020-04-13 12:04:55 +0900

    Add files via upload

---
+++ HW1.cpp
@@ -0,0 +1,22 @@
+﻿// HW1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int, int);
+
+int main() {
+    int a, b;
+    cout << "input two numbers" << endl;
+    cin >> a >> b;
+    cout << endl;
+
+    cout << sum(a, b);
+
+}
+
+int sum(int a, int b) {
+    return a + b;
+}
